9a8f2906b6457029e7d811ae39d0485ae8cb6633
GaIiIeo <49877003+GaIiIeo@users.noreply.github.com>
2019-06-10 10:31:16 -0300

    Teclas (#6)
    me olvide de subirlo antes lmao

---
+++ BINGO FELIPE/BINGOFELIPE/BINGOFELIPE/Form2.Designer.vb
@@ -0,0 +1,77 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCarttel = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtCarttel
+        '
+        Me.txtCarttel.AutoEllipsis = True
+        Me.txtCarttel.AutoSize = True
+        Me.txtCarttel.Font = New System.Drawing.Font("Elephant", 99.74999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCarttel.Location = New System.Drawing.Point(102, 140)
+        Me.txtCarttel.Name = "txtCarttel"
+        Me.txtCarttel.Size = New System.Drawing.Size(527, 192)
+        Me.txtCarttel.TabIndex = 0
+        Me.txtCarttel.Text = "Label1"
+        Me.txtCarttel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.txtCarttel.UseCompatibleTextRendering = True
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 3
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 740.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 102.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.txtCarttel, 1, 1)
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(13, 13)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 3
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 422.0!))
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(941, 602)
+        Me.TableLayoutPanel2.TabIndex = 1
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(966, 627)
+        Me.Controls.Add(Me.TableLayoutPanel2)
+        Me.KeyPreview = True
+        Me.Name = "Form2"
+        Me.Text = "Form2"
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.TableLayoutPanel2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents txtCarttel As Label
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+End Class
